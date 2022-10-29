fe2774f89d12b85942269cfab86920e802acb4c1
Risperwangui <99345570+Risperwangui@users.noreply.github.com>
2022-10-29 15:43:47 +0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,72 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div  className="app">
+      <div id="profile section">
+        <p className="profile"><img  src ="profile_picture2.jpeg" alt="profile picture" id="profile_img"></img></p>
+        <p id="twitter"> Wangui Resper</p>
+        <p id="slack"> Risper Wangui</p>
+      </div>
+      <div id="links section">
+        <div>
+          <a href="https://training.zuri.team/"  target="_blank">
+          <button id="btn_zuri"> Zuri Team </button>
+         Hover me
+          </a>
+        </div>
+        <div>
+          <a href="http://books.zuri.team"  target="_blank">
+            <button id="books">
+              Zuri Books
+            </button>
+            Hover me
+          </a>
+        </div>
+        <div>
+          <a href="https://books.zuri.team/python-for-beginners?ref_id=<Risper Wangui>"  target="_blank">
+            <button id="book_python">
+              Beginners for python
+            </button>
+            Hover me
+          </a>
+        </div>
+        <div>
+          <a href="https://background.zuri.team"  target="_blank">
+            <button id="pitch">
+              Background check on coders
+            </button>
+            Hover me
+          </a>
+        </div>
+        <div>
+          <a href="https://books.zuri.team/design-rules"  target="_blank">
+            <button id="book_design">
+              Design book
+            </button>
+            Hover me
+          </a>
+        </div>
     </div>
-  );
+    </div>
+  
+  )
 }
 
 export default App;
